753713d2ccc02fd515976a0b1f7a5cad4d5a7add
Donnie Guedry <dguedry@gmail.com>
2017-01-11 20:24:14 -0600

    Initial image upload

--- server/methods.js
+++ server/methods.js
@@ -1,8 +1,9 @@
 import { Reaction } from "/server/api";
 import { Packages } from "/lib/collections";
 import { Products } from "/lib/collections";
-import Future from 'fibers/future';
+import { Media } from "/lib/collections";
 import { MagentoImportStatus } from "../lib/collections/schemas";
+import Future from 'fibers/future';
 import MagentoJS from "magentojs"
 
 function updateStatus(value) {
@@ -100,7 +101,9 @@ export const methods = {
         productList.return(products);
       });
 
-      var products=productList.wait().slice(1,3);
+      //TESTING: slice the array and only update 3 products:
+      var products=productList.wait().slice(0,3);
+
       //grab data for each product in list and add to reaction
       products.forEach(function(data) {
         var productInfo = new Future();
@@ -109,35 +112,63 @@ export const methods = {
         });
         var product = productInfo.wait();
         console.log(product);
-        /*var productId = Meteor.call("products/createProduct", (error, productId) => {
-        if (error) {
-        throw new Meteor.Error("create-product-error", error);
-      }
-      return productId;
-    });
-    Meteor.call("products/updateProductField", productId, "label", product.name);
-    */
 
-    var productId = Products.insert({
-      type: "simple", // needed for multi-schema
-      title: product.name,
-      pageTitle: product.name,
-      description: product.description,
-      magento_import: true
-    }, {
-      validate: false
-    }, (error, result) => {
-      // additionally, we want to create a variant to a new product
-      if (result) {
-        Products.insert({
-          ancestors: [result],
-          price: product.price,
-          title: "",
-          type: "variant" // needed for multi-schema
+        var variantId;
+        var productId = Products.insert({
+          type: "simple", // needed for multi-schema
+          title: product.name,
+          pageTitle: product.name,
+          description: product.description,
+          magento_import: true,
+          magento_import_product_id: product.product_id
+        }, {
+          validate: false
+        }, (error, result) => {
+          // additionally, we want to create a variant to a new product
+          if (result) {
+            variantId = Products.insert({
+              ancestors: [result],
+              price: product.price,
+              title: "",
+              type: "variant" // needed for multi-schema
+            });
+          }
         });
-      }
-    });
-    console.log(productId);
+        console.log(productId);
+
+        //images
+        var productImages = new Future();
+        magento.catalog_product_attribute_media.list(product.product_id, function(err, images) {
+          productImages.return(images);
+        })
+        var images = productImages.wait();
+        console.log(images);
+        images.forEach(function(data) {
+          let fileObj;
+          fileObj = new FS.File(data.url);
+          fileObj.metadata = {
+            ownerId: Meteor.userId,
+            productId: productId,
+            variantId: variantId,
+            shopId: Reaction.getShopId(),
+            priority: data.position,
+            toGrid: 1
+          };
+          Media.insert(fileObj);
+        })
+    /*
+    let fileObj;
+    fileObj = new FS.File(file);
+    fileObj.metadata = {
+      ownerId: userId,
+      productId: productId,
+      variantId: variantId,
+      shopId: shopId,
+      priority: count,
+      toGrid: +toGrid // we need number
+    };
+    Media.insert(fileObj);
+    */
   })
   return;
 })
